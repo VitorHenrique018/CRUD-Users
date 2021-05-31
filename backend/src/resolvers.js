8bcf569d140f35b29d48167c8691974ef77055b4
Vitor <vitorhenrique018@gmail.com>
2021-05-31 11:37:40 -0300

    create Edit

--- backend/src/resolvers.js
+++ backend/src/resolvers.js
@@ -8,9 +8,7 @@ module.exports = {
 
   Mutation: {
     createUser: (_, { name, email }) => User.create({ name, email}),
+    editUser: (_, { name, email }) => User.updateOne({ name, email}),
   },
 
-  Subscription: {
-    usersRT: () => User.find(),
-  },
 };
